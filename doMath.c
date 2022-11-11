870c42b225d236e580421549470e33b60724c06b
Bernard Agyeman <40953282+kbagyeman@users.noreply.github.com>
2022-11-11 10:59:38 -0700

    Add files via upload

---
+++ doMath.c
@@ -0,0 +1,22 @@
+#include<time.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+#include "mathtest.h"
+
+int main(void){
+	time_t t;
+	srand((unsigned) time(&t));
+	int rNuma = rand();
+	int rNumb = rand();
+
+	int answer;
+
+	answer = addNumbers(rNuma, rNumb);
+	printf("%d + %d = %d\n",rNuma, rNumb, answer);
+
+	answer = subNumbers(rNuma, rNumb);
+	printf("%d - %d = %d\n",rNuma, rNumb, answer);
+
+	return 0;
+}
